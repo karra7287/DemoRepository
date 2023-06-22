8beb8790f18687fbe949498f210e5802cb8cc50f
arunkumar <karraarunkumar8888@gmail.com>
2023-06-22 13:13:06 +0530

    demorepo

---
+++ src/aruneclipse/pavankalyan.java
@@ -0,0 +1,16 @@
+package aruneclipse;
+
+import java.util.Scanner;
+
+public class pavankalyan {
+
+	public static void main(String[] args) {
+    
+		System.out.println("hiii");
+		Scanner s = new Scanner(System.in);
+		String s1 = s.next();
+		System.out.println(s1);
+
+	}
+
+}
